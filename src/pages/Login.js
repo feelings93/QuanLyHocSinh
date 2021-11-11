ce9a57be14780af5f79d614e2eb0949f2e6ee649
Hào Đinh Việt <vhao1509@gmail.com>
2021-11-11 16:07:05 +0700

    UI login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,9 @@ import Typography from "@mui/material/Typography";
 import useHttp from "../hooks/use-http";
 import { login } from "../lib/api";
 import { CircularProgress } from "@mui/material";
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Container from '@mui/material/Container';
 
 const Login = () => {
   const { sendRequest, status, data, error } = useHttp(login);
@@ -26,12 +29,74 @@ const Login = () => {
   if (data) localStorage.setItem("accessToken", data["access_token"]);
   else localStorage.removeItem("accessToken");
   return (
-    <Box sx={{ width: "100%", height: "100%" }}>
-      <Paper
+  <Grid container sm={12} xs={12}  >
+    <Grid item sm={1}>
+    </Grid>
+    <Grid container item sm={4} xs={12} alignItems="center">
+    
+    <Container fixed>
+    <Box display="flex"  alignItems="center" marginBottom="10px" >
+      <Box sx={{
+        width: "50px",
+        height:"50px",
+        backgroundImage: `url(${"./logo.png"})`, 
+        backgroundSize: "contain",
+        backgroundPosition: 'center',
+        backgroundRepeat: "no-repeat"
+        }}/>
+      <Typography sx={{ fontWeight: 700,fontSize:"30px", marginLeft:"20px", }}>Eduma</Typography>
+    </Box>
+    
+
+    <form onSubmit={loginFormHandler}>
+          <Stack sx={{}} spacing={2} marginBottom="10px">
+            <Typography sx={{ fontWeight: 700,fontSize:"40px" }}>Đăng nhập</Typography>
+            <TextField
+              onChange={(event) => {
+                setEnteredEmail(event.target.value);
+              }}
+              value={enteredEmail}
+              id="email"
+              label="Email*"
+              variant="outlined"
+              color="info"
+            />
+            <TextField
+              onChange={(event) => {
+                setEnteredPassword(event.target.value);
+              }}
+              value={enteredPassword}
+              id="password"
+              label="Mật khẩu*"
+              type="password"
+              variant="outlined"
+              color="info"
+            />
+            <Button size="large" type="submit" variant="contained" sx={{backgroundColor:"#0056fb"}}>
+              {status === "pending" ? <CircularProgress /> : <></>}
+              Đăng nhập
+            </Button>
+          </Stack>
+        </form>
+    </Container>
+    </Grid>
+    <Grid container item sm={7} xs={12} >
+      <Box 
+      sx={{
+        width: "100%",
+        height:"750px",
+        backgroundImage: `url(${"./backgrounddep.jpg"})`, 
+        backgroundSize: "contain",
+        backgroundPosition: "center top",       
+        backgroundRepeat: "no-repeat",
+        }}></Box>
+    </Grid>
+    {/*  <Paper
         sx={{
           margin: "auto",
           maxWidth: "300px",
           transform: "translateY(50%)",
+
         }}
       >
         <form onSubmit={loginFormHandler}>
@@ -62,8 +127,9 @@ const Login = () => {
             </Button>
           </Stack>
         </form>
-      </Paper>
-    </Box>
+            </Paper>*/}
+    </Grid>
+    
   );
 };
 
